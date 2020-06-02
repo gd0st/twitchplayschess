d9b66bc1db316b1badcf04b6587b5594aacb9dce
jbofill10 <juan.bofill10@gmail.com>
2020-06-01 22:59:12 -0400

    Integrate stockfish, python-chess, flask, and flask-socketio Closes #2 Closes #4 Closes #6

---
+++ packages/5head/Game.py
@@ -0,0 +1,26 @@
+import chess
+
+
+class BoardGame:
+    """
+    Wrapper class for Python-Chess.
+    Really the only reason to wrap the python-chess library was to keep a cached state of the game board,
+    since we need to keep stockfish updated every move
+
+    Returns move sequence for frontend to rerender the board positions
+
+    """
+
+    def __init__(self):
+        self.board = chess.Board()
+        self.position = []
+
+    def make_move(self, move):
+        try:
+            move = self.board.push_san(move)
+            # Update position cache
+            self.position.append(move)
+            return move
+        except:
+            return 'Illegal Move'
+
--- packages/5head/Server.py
+++ packages/5head/Server.py
@@ -1,15 +1,38 @@
+from stockfish import Stockfish
 from flask import Flask
+from flask_socketio import SocketIO
+from Game import BoardGame
+import concurrent.futures
 
 app = Flask(__name__)
+app.debug = True
+socketio = SocketIO(app=app, cors_allowed_origins='*')
 
-@app.route("/")
-def main():
-    return "Hello"
+board = BoardGame()
+
+engine = Stockfish(path='stockfish-11-linux/Linux/stockfish_20011801_x64_modern', parameters={
+    'Skill Level': 15
+})
+
+
+@socketio.on("new move")
+def register_move(message):
+    """
+    Gets most popular move from Twitch Driver
+    :param message: the move
+    :return: move sequence to rerender chess positions on frontend
+    """
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        future = executor.submit(board.make_move, message)
+
+        engine.set_position(board.position)
+
+        print(engine.get_board_visual())
+        print("\n\n")
+        print(engine.get_best_move())
+
+        return str(future.result())
 
 
 if __name__ == "__main__":
-    app.run(
-        host='127.0.0.1',
-        port='4000',
-        debug=True
-        )
+    socketio.run(app=app, host='127.0.0.1', port=5000)
