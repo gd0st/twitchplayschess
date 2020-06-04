1114e6401621ac1ecf65d57735b606d576466be6
jbofill10 <juan.bofill10@gmail.com>
2020-06-04 16:40:49 -0400

    Filter only SAN moves to be emitted from Twitch Interface Closes #13

--- packages/backend/app/drivers/stream/TwitchStreamDriver.ts
+++ packages/backend/app/drivers/stream/TwitchStreamDriver.ts
@@ -4,8 +4,28 @@ import {TwitchClient} from '../../clients/TwitchClient'
 export const TwitchStreamDriver : StreamDriver = {
     
     readChat: (sio: any) => {
-
-        var socket = sio('ws://localhost:5000') 
+        
+        let re = new RegExp([
+            // Filters moves that resemble a notated piece to a destination (ke2)
+            '^([1-8kqnb])([1-8abcdefgh])[1-8]$|',
+            // Filters pawn moves to a destination (e4)
+            '^([1-8abcdefgh])[1-8]$|',
+            // Filters moves that contain notated piece taking another piece (qe2xe3)
+            '^([1-8kqnb])([1-8abcdefgh])[1-8][x]([1-8abcdefgh])[1-8]$|',
+            //  Filters pawn taking another piece (e4xd5)
+            '^([1-8abcdefgh])[1-8][x]([1-8abcdefgh])[1-8]$|',
+            // Filters notated piece checking / checkmating king (be2= | #)
+            '^([1-8qnb])([1-8abcdefgh])[1-8][#=]$|',
+            // Pawn checking or checkmating 
+            '^([1-8abcdefgh])[1-8][#=]$|',
+            // Notated piece taking another piece and check / check mating
+            '^([1-8qnb])([1-8abcdefgh])[1-8][x]([1-8abcdefgh])[1-8][#=]$|',
+            // Pawn taking a piece and check / checkmating
+            '^([1-8abcdefgh])[1-8][x]([1-8abcdefgh])[1-8][#=]$|',
+            // Queenside and King side castling
+            '^\\bO-O\\b$|^\\bO-O-O\\b$'].join(''))
+        
+            var socket = sio('ws://localhost:5000') 
 
         socket.on('connect', () => {
             console.log("Connected!")
@@ -36,17 +56,11 @@ export const TwitchStreamDriver : StreamDriver = {
                 TwitchStreamDriver.gameSession = TwitchStreamDriver.gameSession.filter(element => element !== tags.username)
                 return
             }
-            
-            
-            console.log('channel:', channel)
-            console.log('\n\n')
-            console.log('tags:', tags)
-            console.log('\n\n')
-            console.log('message:', message)
-            console.log('\n\n')
-            console.log('self:', self)
 
-            socket.emit('new move', message)
+            if (re.test(message)){
+                socket.emit('new move', message)   
+            }
+           
         });
     },
 
