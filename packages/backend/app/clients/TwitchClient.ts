7f0ec8167786a350569126e2e26043e7f38ba865
Jacob Coughenour <jacob15545@yahoo.com>
2020-06-04 13:49:27 -0400

    replaced dotenv with dotenv-safe for checking .env variables

--- packages/backend/app/clients/TwitchClient.ts
+++ packages/backend/app/clients/TwitchClient.ts
@@ -1,16 +1,17 @@
-import tmi from 'tmi.js'
-import 'dotenv/config'
-import {TmiOptions} from '../definitions/tmi/TmiConfig'
+import 'dotenv-safe/config';
+
+import { TmiOptions } from '../definitions/tmi/TmiConfig';
+import tmi from 'tmi.js';
 
 // Creates instance of twitch client and returns for it to be used
 
-const options:any = new TmiOptions(
-    true, 
-    true, 
-    true, 
-    String(process.env.TWITCH_BOT_NAME), 
-    String(process.env.TWITCH_BOT_ACCESS_TOKEN), 
-    ['alphachamp']
-)
+const options: any = new TmiOptions(
+	true,
+	true,
+	true,
+	process.env.TWITCH_BOT_NAME,
+	process.env.TWITCH_BOT_ACCESS_TOKEN,
+	process.env.TWITCH_BOT_CHANNELS.split(',')
+);
 
 export const TwitchClient = tmi.client(options);
