5d5d8acb2e456389554236f0ec50990e0eadc1dc
Jacob Coughenour <jacob15545@yahoo.com>
2020-06-12 16:12:27 -0400

    add ssl cert loading to 5head

--- packages/5head/Server.py
+++ packages/5head/Server.py
@@ -1,9 +1,11 @@
+from sockets import *
 from flask import Flask
 from flask_socketio import SocketIO
 from Game import BoardGame
 from db.db import SqlDB
+from os import path
 import concurrent.futures
-    
+
 app = Flask(__name__)
 app.debug = True
 
@@ -13,7 +15,11 @@ board = BoardGame()
 db = SqlDB()
 
 # Import socket events after socketio is created
-from sockets import *
 
 if __name__ == "__main__":
-    socketio.run(app=app, host='127.0.0.1', port=5000)
+    certdir = path.join(path.dirname(__file__), '../../certs/')
+    if not path.isdir(certdir):
+        print("certs missing")
+    socketio.run(app=app, host='127.0.0.1', port=5000,
+                 certfile=path.join(certdir, 'server.crt'),
+                 keyfile=path.join(certdir, 'server.key'))
